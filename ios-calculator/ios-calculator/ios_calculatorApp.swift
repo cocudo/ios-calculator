e937c320be4c5e308a11b01e8dbf35e5157196c6
Ivan Pardal <ivanpardalcoca@gmail.com>
2024-03-09 20:19:19 +0100

    Project initial commit.

---
+++ ios-calculator/ios-calculator/ios_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_calculatorApp.swift
+//  ios-calculator
+//
+//  Created by Iván Pardal Coca on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
